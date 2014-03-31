225f6d36a9007ed112b4b23d9b45387477ca6377
vsync <vsync.design@gmail.com>
2014-03-31 12:18:28 +0300

    added "maxlength" support

--- fancyInput.js
+++ fancyInput.js
@@ -84,6 +84,9 @@
 		writer : function(charString, input, appendIndex){
 			var chars = $(input.nextElementSibling).children().not('b'),  // select all characters including <br> (which is a new line)
 				newCharElm = document.createElement('span');
+				
+			if( chars.length > input.maxLength )
+				return this;
 			
 			if( charString == ' ' ) // space
 				charString = '&nbsp;';
